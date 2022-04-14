9b031003842f4f5be768543604a5b993f978a8f5
deadmen107 <yildiz655@gmail.com>
2022-04-14 22:24:15 +0300

    test

---
+++ wpfapp5/Service/GlobalStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StarNote.Model;
+
+namespace StarNote.Service
+{
+    public class GlobalStore
+    {
+        public GlobalStore()
+        {
+            Maindataorder = new List<OrderModel>();
+            MaindataCostumer = new List<CostumerOrderModel>();
+            MaindataJoborder = new List<JobOrderModel>();
+        }
+        public static List<OrderModel> Maindataorder;
+        public static List<CostumerOrderModel> MaindataCostumer;
+        public static List<JobOrderModel> MaindataJoborder;
+    }
+}
